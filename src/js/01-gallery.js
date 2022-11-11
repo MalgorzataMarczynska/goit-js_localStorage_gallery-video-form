4799b46035daccd64912774dccfcc30ab1bc3581
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2022-11-11 15:10:48 +0100

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const simplelightbox = require('simplelightbox');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="" alt="Large image"/>
+    </div>
+`);
+
+const galleryMark = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item"><a class="gallery__link" href="${galleryItem.original}"><img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}" /></a></div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('afterbegin', galleryMark);
 console.log(galleryItems);
