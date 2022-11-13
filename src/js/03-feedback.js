26a971d1a261212c4ba4b08b5394a92ff41eb826
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2022-11-13 19:20:24 +0100

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,63 +1,53 @@
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
-const input = form.querySelector('input');
-const message = form.querySelector('textarea');
-const btn = form.querySelector('button');
+const input = document.querySelector('input');
+const message = document.querySelector('textarea');
+const btn = document.querySelector('button');
 
-// updateOutput();
-// form.addEventListener('submit', saveMessage);
+form.addEventListener('input', throttle(savingData, 500));
+function savingData(event) {
+  try {
+    event.preventDefault();
+    const {
+      elements: { email, message },
+    } = event.currentTarget;
+    const user = {
+      email: email.value,
+      message: message.value,
+    };
+    const serializedUser = JSON.stringify(user);
+    localStorage.setItem('feedback-form-state', serializedUser);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function loadingData() {
+  const userData = localStorage.getItem('feedback-form-state');
+  const deserializedUser = JSON.parse(userData);
+  try {
+    if (userData === null) {
+      return (input.value = '');
+    }
+    input.value = deserializedUser.email;
+    message.value = deserializedUser.message;
+  } catch (error) {
+    console.log(error);
+    return userData;
+  }
+}
 
-// function saveMessage(evt) {
-//   evt.preventDefault();
-//   localStorage.setItem(feedback-form-state, form.elements.message.value);
-//   updateOutput();
-//   form.reset();
-// }
-
-// function updateOutput() {
-//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || '';
-// }
-
+loadingData();
 form.addEventListener('submit', handleSubmit);
-
 function handleSubmit(event) {
   event.preventDefault();
   const {
     elements: { email, message },
   } = event.currentTarget;
-
-  if (email.value === '' || message.value === '') {
-    return alert('Please fill in all the fields!');
-  }
   const user = {
     email: email.value,
     message: message.value,
-    // showThis() {
-    //   console.log(this);
   };
-  const key = 'feedback-form-state';
-
-  const save = (key, user) => {
-    try {
-      const serializedState = JSON.stringify(user);
-      localStorage.setItem(key, serializedState);
-    } catch (error) {
-      console.error('Set state error: ', error.message);
-    }
-  };
-  const load = key => {
-    try {
-      const serializedState = localStorage.getItem(key);
-      return serializedState === null ? undefined : JSON.parse(serializedState);
-    } catch (error) {
-      console.error('Get state error: ', error.message);
-    }
-  };
-  // export default {
-  //   save,
-  //   load,
-  // };
   console.log(`E-mail: ${email.value}, Message: ${message.value}`);
-  user.showThis();
   event.currentTarget.reset();
-  //localStorage.removeItem("user");
+  localStorage.removeItem('feedback-form-state');
 }
