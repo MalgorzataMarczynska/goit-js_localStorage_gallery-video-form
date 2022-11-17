1de5c9e5f16479fff93fbd4644f750bc036a75f6
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2022-11-17 22:51:40 +0100

    video_bugfix2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -32,9 +32,31 @@ const restorePlayerToLastPlay = () => {
   player.setCurrentTime(lastPlayTime);
 };
 restorePlayerToLastPlay();
-const resetTime = ({ duration, percent, seconds }) => {
+
+// const resetTime = ({ duration, percent, seconds }) => {
+//   const lastPlayTime = getLocalLastPlayedTime();
+//   if (lastPlayTime === duration - 1) {
+//     localStorage.removeItem('videoplayer-current-time');
+//   }
+// };
+
+const resetTime = () => {
   const lastPlayTime = getLocalLastPlayedTime();
-  if (lastPlayTime === duration) {
+  const localLastPlayTime = localStorage.getItem('videoplayer-current-time');
+  const basedTime = 0;
+  if (localLastPlayTime > 570) {
     localStorage.removeItem('videoplayer-current-time');
   }
+  if (
+    localStorage.getItem('videoplayer-current-time') === null ||
+    localStorage.getItem('videoplayer-current-time') === undefined
+  ) {
+    console.log('I want to back to beginning!');
+    restorePlayerToLastPlay();
+    player.on('play', () => {
+      console.log('playing the video once again!');
+      player.setCurrentTime(basedTime);
+    });
+  }
 };
+player.on('ended', resetTime);
